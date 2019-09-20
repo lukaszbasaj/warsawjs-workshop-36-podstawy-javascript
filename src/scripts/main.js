ad6568dafcee6bfc08b0ada846038e1b77970f71
Łukasz Basaj <lbasaj@gmail.com>
2019-09-20 18:21:47 +0200

    Add previous step

---
+++ src/scripts/main.js
@@ -0,0 +1,22 @@
+  
+import '../styles/main.scss';
+
+import { createLogo, createMainContainer, createButton } from './ui';
+
+function main() {
+    const app = document.getElementById('root');
+
+    const logo = createLogo();
+    app.appendChild(logo);
+
+    const container = createMainContainer();
+    app.appendChild(container);
+
+    const button = createButton();
+    container.appendChild(button);
+
+    switchSound();
+
+}
+
+window.addEventListener('DOMContentLoaded', main);
